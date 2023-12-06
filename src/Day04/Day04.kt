a18b3e022340452b46ed8b818d5f47a1bb8457fa
Haley Raizes <hraizes99@gmail.com>
2023-12-05 19:10:14 -0800

    Save wip on day4 part2

--- src/Day04/Day04.kt
+++ src/Day04/Day04.kt
@@ -3,23 +3,39 @@ package Day04
 import readInput
 import kotlin.math.pow
 
+
+// TODO Have not finished part 2
+
 class ScratchCard(
-    private val cardId: Int,
+    val cardId: Int,
     private val winningNumbers: Set<Int>,
-    private val cardNumbers: Set<Int>
+    private val cardNumbers: Set<Int>,
+    private val winners: Set<Int> = winningNumbers.intersect(cardNumbers)
 ) {
     fun calculateTotalPoints(): Int {
         val winningNumberCount = getWinners().size
-        if(winningNumberCount < 2) {
-            return winningNumberCount
-        }
+        if(winningNumberCount < 2) { return winningNumberCount }
         return "2".toDouble().pow(winningNumberCount-1).toInt()
     }
 
-    private fun getWinners(): Set<Int> {
-        return winningNumbers.intersect(cardNumbers)
+    private fun getWinners() = winners
+
+    fun getCopies(maxCopyId: Int): List<Int> {
+        val endRange = cardId + winners.size
+        if(endRange > maxCopyId) {
+            return emptyList()
+        }
+        return (cardId + 1 ..endRange).toList()
     }
+
 }
+
+
+
+// Card 1 -> 4 winning numbers -> copies = 2, 3, 4, 5
+// Copies 2, 3, 4, 5
+    // Copy 2 -> 2 winning numbers -> copies 3, 4
+
 object Day04Solver {
 
     fun solvePart1(input: List<String>): Int {
@@ -27,8 +43,32 @@ object Day04Solver {
     }
 
     fun solvePart2(input: List<String>): Int {
-        return 0
+        var total = 0
+        val scratchCards = input.map { parseCard(it) }
+        total += scratchCards.size
+        val maxCardId = scratchCards.last().cardId
+        val copiesById = scratchCards.associate {it.cardId to it.getCopies(maxCardId)}
+        println(copiesById)
+        scratchCards.forEach { originalCard ->
+            val cardCopies = originalCard.getCopies(maxCardId)
+            // 2, 3, 4, 5
+
+        }
+        println("total $total")
+        return total
     }
+
+    private fun calculateCopyCount(cardId: Int, copiesByCardId: Map<Int, List<Int>>, runningTotal: Int = 0): Int {
+        val copyIds = copiesByCardId[cardId]!!
+        if (copyIds.isEmpty()) return runningTotal
+        println("runningTotal: $runningTotal")
+        copyIds.forEach {
+            val totalSoFar = runningTotal + copyIds.size
+            calculateCopyCount(it, copiesByCardId, totalSoFar)
+        }
+        return runningTotal
+    }
+
 
     private fun parseCard(value: String): ScratchCard {
         val allNumbers = value.split(":").last().split("|")
@@ -53,14 +93,14 @@ fun main() {
 
     val day4ExampleInput = readInput("Day04_Example")
     check(Day04Solver.solvePart1(day4ExampleInput) == 13)
-    //check(Day04Solver.solvePart2(day4ExampleInput) == -1)
+    check(Day04Solver.solvePart2(day4ExampleInput) == 30)
 
     val myPersonalInput = readInput("Day04")
 
     val part1Answer = Day04Solver.solvePart1(myPersonalInput)
     println("Part 1: $part1Answer")
-
-    val part2Answer = Day04Solver.solvePart2(myPersonalInput)
-    println("Part 2: $part2Answer")
+//
+//    val part2Answer = Day04Solver.solvePart2(myPersonalInput)
+//    println("Part 2: $part2Answer")
 
 }
