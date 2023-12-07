ece596b9cdc05c0ac9218fba9d8689f510aa3582
Haley Raizes <hraizes99@gmail.com>
2023-12-06 22:58:28 -0700

    Day04Part2 done

--- src/Day04/Day04.kt
+++ src/Day04/Day04.kt
@@ -4,14 +4,14 @@ import readInput
 import kotlin.math.pow
 
 
-// TODO Have not finished part 2
-
 class ScratchCard(
     val cardId: Int,
     private val winningNumbers: Set<Int>,
     private val cardNumbers: Set<Int>,
-    private val winners: Set<Int> = winningNumbers.intersect(cardNumbers)
 ) {
+
+    private val winners: Set<Int> = winningNumbers.intersect(cardNumbers)
+
     fun calculateTotalPoints(): Int {
         val winningNumberCount = getWinners().size
         if(winningNumberCount < 2) { return winningNumberCount }
@@ -19,6 +19,8 @@ class ScratchCard(
     }
 
     private fun getWinners() = winners
+    private fun getWinningNumbers() = winningNumbers
+    private fun getCardNumbers() = cardNumbers
 
     fun getCopies(maxCopyId: Int): MutableList<Int> {
         val endRange = cardId + winners.size
@@ -30,12 +32,6 @@ class ScratchCard(
 
 }
 
-
-
-// Card 1 -> 4 winning numbers -> copies = 2, 3, 4, 5
-// Copies 2, 3, 4, 5
-    // Copy 2 -> 2 winning numbers -> copies 3, 4
-
 object Day04Solver {
 
     fun solvePart1(input: List<String>): Int {
@@ -44,16 +40,17 @@ object Day04Solver {
 
     fun solvePart2(input: List<String>): Int {
         val scratchCards = input.map { parseCard(it) }
-        val maxCardId = scratchCards.last().cardId
-        val copiesById = scratchCards.associate {it.cardId to it.getCopies(maxCardId)}
-        println(copiesById)
-        scratchCards.forEach { originalCard ->
-            val cardCopies = originalCard.getCopies(maxCardId)
-            cardCopies.forEach {
-                copiesById[it]?.addAll(copiesById[it] ?: emptyList())
+        val maxCardId = scratchCards.size
+        val copiesById = scratchCards.associate { it.cardId to it.getCopies(maxCardId) }
+        // Starting copies for each original card
+        val aggregatedCopiesById = scratchCards.associate { it.cardId to it.getCopies(maxCardId) }
+        aggregatedCopiesById.values.forEach { copies ->
+            copies.forEach { cardId ->
+                aggregatedCopiesById[cardId]?.addAll(copiesById[cardId] ?: emptyList())
             }
         }
-        return copiesById.values.sumOf { it.size } + scratchCards.size
+        // Sum original cards and all the copies
+        return scratchCards.size + aggregatedCopiesById.values.sumOf { it.size }
     }
 
 
@@ -80,7 +77,7 @@ fun main() {
 
     val day4ExampleInput = readInput("Day04_Example")
     check(Day04Solver.solvePart1(day4ExampleInput) == 13)
-    //check(Day04Solver.solvePart2(day4ExampleInput) == 30)
+    check(Day04Solver.solvePart2(day4ExampleInput) == 30)
 
     val myPersonalInput = readInput("Day04")
 
